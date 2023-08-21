6454443e133387c39aba4e8863e4b9ea1b4c9d25
Austyn Harriman <aeharrima@gmail.com>
2023-08-21 17:00:00 -0500

    notes

--- src/main/java/com/example/helloworld/config/GlobalErrorHandler.java
+++ src/main/java/com/example/helloworld/config/GlobalErrorHandler.java
@@ -19,17 +19,25 @@ import org.springframework.web.servlet.function.ServerResponse;
 import com.example.helloworld.models.ErrorMessage;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+// @RestControllerAdvice is an annotation used to define @ExceptionHandler, @InitBinder, and @ModelAttribute methods
+// that apply to all @RequestMapping methods. It's a way to apply exception handling globally across multiple controllers.
 @RestControllerAdvice
 public record GlobalErrorHandler(ObjectMapper mapper) {
 
+  // This method handles the NoHandlerFoundException, which is thrown when a request is made to an endpoint
+  // that doesn't exist in the application. It returns a 404 with a body of a custom error message with a "Not Found" message.
   @ResponseStatus(HttpStatus.NOT_FOUND)
   @ExceptionHandler(NoHandlerFoundException.class)
   public ErrorMessage handleNotFound(final HttpServletRequest request, final Exception error) {
     return ErrorMessage.from("Not Found");
   }
 
+  // @Cacheable is an annotation that indicates that the result of invoking a method (in this case, handleInternalError)
+  // can be cached. The next time the method is called with the same arguments, the cached result is returned
+  // instead of invoking the method again. This can improve performance by avoiding unnecessary computations.
   @Cacheable
   public ServerResponse handleInternalError(final Throwable error, final ServerRequest request) {
+    // This method handles internal server errors and returns a custom error message with the error's message.
     return ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR)
       .body(ErrorMessage.from(error.getMessage()));
   }
@@ -40,6 +48,8 @@ public record GlobalErrorHandler(ObjectMapper mapper) {
     final HttpServletResponse response,
     final AuthenticationException error
   ) throws IOException {
+    // This method handles authentication errors. When a user tries to access a protected resource without
+    // proper authentication, this method sends a custom error message with a "Requires authentication" message.
     final var errorMessage = ErrorMessage.from("Requires authentication");
     final var json = mapper.writeValueAsString(errorMessage);
 
